830e1d1d9a9159e8b82847ac53dbe3d3c8280d3b
Proma <promaafia@gmail.com>
2019-10-18 23:25:24 +0600

    STEP #3.Makes improvements to variable names

--- StudentList.java
+++ StudentList.java
@@ -4,24 +4,26 @@ import java.util.*;
 public class StudentList {
 	public static void main(String[] args) 
 	{
-
 //		Check arguments
             if (args == null || args.Length != 1)
             {
                 Console.WriteLine("Usage: dotnet dev275x.rollcall.dll (a | r | c | +WORD | ?WORD)");
                 return; // Exit early.
             }
-
 		if(args[0].equals("a")) 
 		{
 			System.out.println("Loading data ...");			
 			try {
 			BufferedReader s = new BufferedReader(
+			BufferedReader fileStream = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
 			String r = s.readLine();
 			String i[] = r.split(",");			
 			for(String j : i) { System.out.println(j);
+			String reader = fileStream.readLine();
+			String words[] = reader.split(",");			
+			for(String word: reader) { System.out.println(j);
                                }
 			            } 
                  catch (Exception e){} 
@@ -33,6 +35,7 @@ public class StudentList {
 			try 
 			{
 			BufferedReader s = new BufferedReader(
+			BufferedReader fileStream = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt")));  
 			String r = s.readLine();
@@ -40,6 +43,11 @@ public class StudentList {
 			Random x = new Random();
 				int y = x.nextInt(i.length);
 					System.out.println(i[y]);
+			String reader = fileStream.readLine();
+			String words[] = reader.split(",");	
+			Random rand = new Random();
+				int randomIndex = rand.nextInt(words.length);
+					System.out.println(words[y]);
 			} 
 			catch (Exception e){} 
 			System.out.println("Data Loaded.");			
@@ -50,16 +58,22 @@ public class StudentList {
 			try
 			 {
 			BufferedWriter s = new BufferedWriter(
+			BufferedWriter fileStream = new BufferedWriter(
 					new FileWriter("students.txt", true));
 			String t = args[0].substring(1);
 	        Date d = new Date();
+			String argValue = args[0].substring(1);
+	        Date fileContents = new Date();
 	        String df = "dd/mm/yyyy-hh:mm:ss a";
 	        DateFormat dateFormat = new SimpleDateFormat(df);
 	        String fd= dateFormat.format(d);
 			s.write(", "+t+"\nList last updated on "+fd);
 			s.close();
+	        String fd= dateFormat.format(fileContents);
+			fileStream.write(", "+t+"\nList last updated on "+fd);
+			fileStream.close();
 			} catch (Exception e){}
-							
+
 			System.out.println("Data Loaded.");	
 		}
 		else if(args[0].contains("?")) 
@@ -67,15 +81,20 @@ public class StudentList {
 			System.out.println("Loading data ...");			
 			try {
 			BufferedReader s = new BufferedReader(
+			BufferedReader fileStream = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
 			String r = s.readLine();
 			String i[] = r.split(",");	
+			String reader = fileStream.readLine();
+			String words[] = reader.split(",");	
 			boolean done = false;
 			String t = args[0].substring(1);
+			String argValue = args[0].substring(1);
 			for(int idx = 0; idx<i.length && !done; idx++)
 			 {
 				if(i[idx].equals(t)) {
+				if(words[idx].equals(argValue)) {
 					System.out.println("We found it!");
 						done=true;
 				}
@@ -83,18 +102,23 @@ public class StudentList {
 			} catch (Exception e){} 
 			System.out.println("Data Loaded.");				
 		}
+
 		else if(args[0].contains("c")) 
 		{
 			System.out.println("Loading data ...");			
 			try {
 			BufferedReader s = new BufferedReader(
+			BufferedReader fileStream = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
 			String D = s.readLine();
 			char a[] = D.toCharArray();			
+			String fileContents = fileStream.readLine();
+			char characters[] = fileContents.toCharArray();			
 			boolean in_word = false;
 			int count=0;
 			for(char c:a) {
+			for(char c:characters) {
 				if(c ==' ') 
 				{
 					if (!in_word) {	count++; in_word =true;
@@ -103,6 +127,7 @@ public class StudentList {
 				}
 			}
 			System.out.println(count +" word(s) found " + a.length);
+			System.out.println(count +" word(s) found " + characters.length);
 			} catch (Exception e){} 
 			System.out.println("Data Loaded.");				
 		}
