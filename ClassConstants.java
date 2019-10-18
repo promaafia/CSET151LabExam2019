56810f5cf10fc73e8ef4d51b36c85776604bdd5d
Proma <promaafia@gmail.com>
2019-10-18 23:54:37 +0600

    STEP #9.Adds handling for case when user enters invalid arguments

---
+++ ClassConstants.java
@@ -0,0 +1,14 @@
+public  final  class  Constants {
+    public  static final  String  StartDialog = "Loading data ...";
+    public  static  final  String DataFile = "students.txt";
+    public  static  final  String EndDialog = "Data Loaded.";
+    public  static  final  String Usage = "Usage: java ase.StudentList (a|r|c|?word|+word)";
+    public  static  final  String Success = "We found it!";
+    public  static  final  String ShowAll = "a";
+    public  static final  String ShowRandow = "r";
+    public  static  final  String count = "c";
+    public  static final  String addWord = "+";
+    public  static  final  String query = "?";
+    public  static final String DateFormat = "dd/mm/yyyy-hh:mm:ss a";
+    public  static  final  String InvalidMsg = "Invalid Argument";
+}
